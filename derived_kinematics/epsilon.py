8ed69d1c637bf0100c02381acc23242b6bb7d8fc
ErenMcLaren <dima.watkins@gmail.com>
2024-04-26 12:51:18 -0400

    Added derived kinematics and statics.

---
+++ derived_kinematics/epsilon.py
@@ -0,0 +1,46 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+    
+def calculate_kinematics_epsilon(
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    verbose: bool = True) -> float:
+    """
+    Description
+    --------------
+    Calculate epsilon, which is just a ratio of kinematic quantities:
+    \epsilon := 2 * m_{p} * x_{B} / Q
+
+    Parameters
+    --------------
+    squared_Q_momentum_transfer: (float)
+        kinematic momentum transfer to the hadron. 
+
+    x_Bjorken: (float)
+        kinematic Bjorken X
+
+    verbose: (bool)
+        Debugging console output.
+    
+
+    Notes
+    --------------
+    """
+            
+    try:
+
+        # (1): Calculate Epsilon right away:
+        epsilon = (2. * x_Bjorken * _MASS_OF_PROTON_IN_GEV) / np.sqrt(squared_Q_momentum_transfer)
+
+        # (1.1): If verbose, print the result:
+        if verbose:
+            print(f"> Calculated epsilon to be: {epsilon}")
+
+        # (2): Return Epsilon:
+        return epsilon
+    
+    except Exception as ERROR:
+        print(f"> Error in computing kinematic epsilon:\n> {ERROR}")
+        return 0.
---
+++ derived_kinematics/epsilon_squared.py
@@ -0,0 +1,46 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+    
+def calculate_kinematics_epsilon_squared(
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    verbose = True) -> float:
+    """
+    Description
+    --------------
+    Calculate epsilon, which is just a ratio of kinematic quantities:
+    \epsilon^{2} := 4 * m_{p}^{2} * x_{B}^{2} / Q^{2}
+
+    Parameters
+    --------------
+    squared_Q_momentum_transfer: (float)
+        kinematic momentum transfer to the hadron
+
+    x_Bjorken: (float)
+        kinematic Bjorken X
+
+    verbose: (bool)
+        Debugging console output.
+    
+
+    Notes
+    --------------
+    """
+
+    try:
+
+        # (1): Calculate it right away:
+        epsilon_squared = (4. * _MASS_OF_PROTON_IN_GEV**2 * x_Bjorken**2) / squared_Q_momentum_transfer
+
+        # (1.1): If verbose, print the result:
+        if verbose:
+            print(f"> Calculated epsilon squared to be: {epsilon_squared}")
+
+        # (2): Return Epsilon:
+        return epsilon_squared
+    
+    except Exception as ERROR:
+        print(f"> Error in computing kinematic epsilon squared:\n> {ERROR}")
+        return 0.
