6211d1a3a397189aaf564f830906ea92f1822e55
ErenMcLaren <dima.watkins@gmail.com>
2024-05-27 16:05:51 -0400

    Added c1,unp for the BH amplitude squared. Mass of the proton is still not defined.

---
+++ coefficients/bh_coefficients/unpolarized/unpolarized_c1_bh.py
@@ -0,0 +1,75 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+
+def calculate_c_1_unpolarized_bh(
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    squared_hadronic_momentum_transfer_t: float,
+    epsilon: float,
+    lepton_energy_fraction_y: float,
+    k_shorthand: float,
+    Dirac_form_factor_F1: float, 
+    Pauli_form_factor_F2: float, 
+    verbose: bool = True) -> float:
+    """
+    Description
+    --------------
+    Equation (35) of the BH02 Formalism.
+
+    Parameters
+    --------------
+    squared_Q_momentum_transfer: (float)
+
+    x_Bjorken: (float)
+
+    squared_hadronic_momentum_transfer_t: (float)
+
+    epsilon: (float)
+
+    lepton_energy_fraction_y: (float)
+
+    k_shorthand: (float)
+
+    Dirac_form_factor_F1: (float)
+
+    Pauli_form_factor_F2: (float)
+
+    verbose: (bool)
+        Debugging console output.
+
+    Notes
+    --------------
+    (1): This coefficient is in Equation (35) from
+        this paper:
+        https://arxiv.org/pdf/hep-ph/0112108.pdf
+    """
+
+    try:
+
+        # (1): Calculate the common appearance of F1 + F2:
+        addition_of_form_factors_squared = (Dirac_form_factor_F1 + Pauli_form_factor_F2)**2
+
+        # (2): Calculate the common appearance of a weighted sum of F1 and F2:
+        weighted_combination_of_form_factors = Dirac_form_factor_F1**2 - ((squared_hadronic_momentum_transfer_t / (4. * _MASS_OF_PROTON_IN_GEV**2)) * Pauli_form_factor_F2**2)
+        
+        # (3):  The first part of the first line:
+        first_line_first_part = ((4. * x_Bjorken**2 * _MASS_OF_PROTON_IN_GEV**2 / squared_hadronic_momentum_transfer_t) - 2. * x_Bjorken - epsilon**2) * weighted_combination_of_form_factors
+        
+        # (4): The first part of the second line:
+        first_line_second_part = 2. * x_Bjorken**2 * (1. - (1. - 2. * x_Bjorken) * (squared_hadronic_momentum_transfer_t / squared_Q_momentum_transfer)) * addition_of_form_factors_squared
+
+        # (5): Multiply by the prefactor to obtain c^{(1)}_{BH}
+        c1_unpolarized_BH = 8. * k_shorthand * (2. - lepton_energy_fraction_y) * (first_line_first_part + first_line_second_part)
+        
+        # (5.1): If verbose, log the output:
+        if verbose:
+            print(f"> Calculated c1_unpolarized_BH to be: {c1_unpolarized_BH}")
+
+        # (6): Return the coefficient:
+        return c1_unpolarized_BH
+    
+    except Exception as E:
+        print(f"> Error in computing c1_unpolarized_BH:\n> {E}")
+        return 0.
