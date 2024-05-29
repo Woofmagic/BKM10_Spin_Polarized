8cba358002741a82d0a6e49674ec4110265be4f2
ErenMcLaren <dima.watkins@gmail.com>
2024-05-29 15:05:11 -0400

    Updated the plot_results function with some preparatory data for plotting later.

--- calculation/plot_results.py
+++ calculation/plot_results.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-def plot_cross_section():
+from utilities.plotting.plot_customizer import PlotCustomizer
+
+def plot_cross_section(lab_azimuthal_phi, calculated_cross_section):
     
     # (1): Figure instance:
     figure = plt.figure(figsize = (18, 6))
 
     # (2): Add an Axes Object:
-    axes_1 = figure.add_subplot(131)
-    axes_2 = figure.add_subplot(132)
-    axes_3 = figure.add_subplot(133, projection = '3d')
+    axes_object = figure.add_subplot(131)
 
     # (3): Obtain the data:
     values_of_x_B = jlab_pandas_df['x_b']
@@ -16,9 +16,13 @@ def plot_cross_section():
     values_of_t = jlab_pandas_df['t']
     
     customized_plot = PlotCustomizer(
-        axes_3,
+        axes_object,
         title = r"Kinematic Phase Space for JLab Data",
-        xlabel = r"$x_{B}$",
-        ylabel = r"$Q^{2}$",
-        zlabel = r"$t$",
+        xlabel = r"$\phi [\text{deg}]$",
+        ylabel = r"$d^{4} \sigma [\text{nb}/\text{GeV}^{{4}}]$",
         grid = True)
+    
+    customized_plot.add_scatter_plot(
+        x_data = lab_azimuthal_phi,
+        y_data = calculated_cross_section,
+        color = 'blue')
