58812e9bca33a7bbbcc87513fc0454d59fc59510
ErenMcLaren <dima.watkins@gmail.com>
2024-05-29 10:52:24 -0400

    Added the coefficient C0+,unp(n = 0). That's better notation anyway.

---
+++ coefficients/interference_coefficients/unpolarized/unpolarized_C0p0.py
@@ -0,0 +1,37 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+
+def calculate_c_0_zero_plus_unpolarized(
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    squared_hadronic_momentum_transfer_t: float,
+    epsilon: float,
+    lepton_energy_fraction_y: float, 
+    shorthand_k: float,
+    verbose: bool = True) -> float:
+    """
+    """
+
+    try:
+
+        # (1): Calculate the bracket quantity:
+        bracket_quantity = epsilon**2 + squared_hadronic_momentum_transfer_t * (2. - 6.* x_Bjorken - epsilon**2) / (3. * squared_Q_momentum_transfer)
+        
+        # (2): Calculate part of the prefactor:
+        prefactor = 12. * np.sqrt(2.) * shorthand_k * (2. - lepton_energy_fraction_y) * np.sqrt(1. - lepton_energy_fraction_y - (epsilon**2 * lepton_energy_fraction_y**2 / 4)) / np.power(1. + epsilon**2, 2.5)
+        
+        # (3): Calculate the coefficient:
+        c_0_zero_plus_unp = prefactor * bracket_quantity
+        
+        # (3.1): If verbose, log the output:
+        if verbose:
+            print(f"> Calculated c_0_zero_plus_unp to be: {c_0_zero_plus_unp}")
+
+        # (4): Return the coefficient:
+        return c_0_zero_plus_unp
+
+    except Exception as ERROR:
+        print(f"> Error in calculating c_0_zero_plus_unp for Interference Term:\n> {ERROR}")
+        return 0.
