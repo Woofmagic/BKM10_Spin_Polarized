77418c2a646778a920a617dabba19f83a196b592
ErenMcLaren <dima.watkins@gmail.com>
2024-08-15 14:31:12 -0400

    Coded up the LP Smp n = 2, LP Smp n = 3, and SmpV n = 1 coefficients.

---
+++ coefficients/interference_coefficients/lp_polarized/lp_polarized_Smp1V.py
@@ -0,0 +1,62 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+
+def calculate_s_1_minus_plus_longitudinally_polarized_V(
+    target_polarization: float,
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    squared_hadronic_momentum_transfer_t: float,
+    epsilon: float,
+    lepton_energy_fraction_y: float, 
+    shorthand_K: float,
+    verbose: bool = True) -> float:
+    """
+    """
+
+    try:
+
+        # (1): Calculate the recurrent quantity sqrt(1 + epsilon^2):
+        root_one_plus_epsilon_squared = np.sqrt(1. + epsilon**2)
+
+        # (2): Calculate t/Q^{2}:
+        t_over_Q_squared = squared_hadronic_momentum_transfer_t / squared_Q_momentum_transfer
+
+        # (3): Calculate 1 + sqrt(1 + epsilon^2):
+        fancy_epsilon_term = 1. + root_one_plus_epsilon_squared
+
+        # (4): Calculate the prefactor to the first bracket term:
+        prefactor_first_bracket_term = 2. - 2. * lepton_energy_fraction_y + lepton_energy_fraction_y**2 + lepton_energy_fraction_y**2 * epsilon**2 / 2.
+
+        # (5): Calculate the first bracket term main stuff:
+        main_first_bracket_term = 3. + 2. * epsilon**2 + root_one_plus_epsilon_squared - 2. * x_Bjorken * fancy_epsilon_term - t_over_Q_squared * (1. - 2. * x_Bjorken) * (1. - 2. * x_Bjorken - root_one_plus_epsilon_squared)
+
+        # (6): Calculate the prefactor for the second bracket termL
+        prefactor_second_bracket_term = 1. - lepton_energy_fraction_y - lepton_energy_fraction_y**2 * epsilon**2 / 4.
+        
+        # (7): Calculate the main stuff in second bracket term:
+        second_part_main_second_bracket_term = -2. * t_over_Q_squared * (2. * fancy_epsilon_term - epsilon**2 - 12. * x_Bjorken * (1. - x_Bjorken) - 4. * x_Bjorken * root_one_plus_epsilon_squared)
+        
+        # (8): Calculate the first part of the second bracket term:
+        first_part_main_second_bracket_term = 8. + 5. * epsilon**2 - 2. * x_Bjorken * (3. - root_one_plus_epsilon_squared)
+
+        # (9): Calculate the bracket term in full:
+        bracket_term = prefactor_first_bracket_term * main_first_bracket_term + prefactor_second_bracket_term * (first_part_main_second_bracket_term + second_part_main_second_bracket_term)
+        
+        # (10): Calculate the prefactor:
+        prefactor = -4. * target_polarization * shorthand_K * t_over_Q_squared / root_one_plus_epsilon_squared**6
+        
+        # (11): Calculate entire coefficient in one:
+        s_1_minus_plus_LP_V = prefactor * bracket_term
+        
+        # (8.1): If verbose, log the output:
+        if verbose:
+            print(f"> Calculated s_1_minus_plus_LP_V to be:\n{s_1_minus_plus_LP_V}")
+
+        # (9): Return the coefficient:
+        return s_1_minus_plus_LP_V
+
+    except Exception as ERROR:
+        print(f"> Error in s_1_minus_plus_LP_V s_1_minus_plus_LP for Interference Term:\n> {ERROR}")
+        return 0
---
+++ coefficients/interference_coefficients/lp_polarized/lp_polarized_Smp2.py
@@ -0,0 +1,52 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+
+def calculate_s_2_minus_plus_longitudinally_polarized(
+    target_polarization: float,
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    squared_hadronic_momentum_transfer_t: float,
+    epsilon: float,
+    lepton_energy_fraction_y: float, 
+    verbose: bool = True) -> float:
+    """
+    """
+
+    try:
+
+        # (1): Calculate the recurrent quantity sqrt(1 + epsilon^2):
+        root_one_plus_epsilon_squared = np.sqrt(1. + epsilon**2)
+
+        # (2): Calculate t/Q^{2}:
+        t_over_Q_squared = squared_hadronic_momentum_transfer_t / squared_Q_momentum_transfer
+
+        # (3): Calculate one plus sqrt(1 + epsilon^2):
+        fancy_epsilon_term = 1. + root_one_plus_epsilon_squared
+
+        # (4): Calculate the first bracket term:
+        first_bracket_term = t_over_Q_squared * (2. * fancy_epsilon_term + epsilon**2 * root_one_plus_epsilon_squared - x_Bjorken * (3. * fancy_epsilon_term - epsilon**2))
+
+        # (5): Calculate the second term:
+        second_bracket_term = t_over_Q_squared**2 * (epsilon**2 - 2. * x_Bjorken**2 * (2. + root_one_plus_epsilon_squared) + x_Bjorken * (3. - epsilon**2 + root_one_plus_epsilon_squared))
+
+        # (6): Combine all the terms:
+        entire_bracket_term = first_bracket_term + second_bracket_term + epsilon**2 * fancy_epsilon_term
+
+        # (7): Calculate the prefactor:
+        prefactor = -4. * target_polarization (2. - lepton_energy_fraction_y) * (1. - lepton_energy_fraction_y - lepton_energy_fraction_y**2 * epsilon**2 / 4.) / root_one_plus_epsilon_squared**6
+
+        # (8): Calculate the coefficient:
+        s_2_minus_plus_LP = prefactor * entire_bracket_term
+
+        # (8.1): If verbose, log the output:
+        if verbose:
+            print(f"> s_2_minus_plus_LP s_2_minus_plus_LP to be:\n{s_2_minus_plus_LP}")
+
+        # (9): Return the coefficient:
+        return s_2_minus_plus_LP
+
+    except Exception as ERROR:
+        print(f"> Error in calculating s_2_minus_plus_LP for Interference Term:\n> {ERROR}")
+        return 0.
