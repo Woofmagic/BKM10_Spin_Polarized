f59f84a6fe2877300371db9740ccd356eea13202
ErenMcLaren <dima.watkins@gmail.com>
2024-09-03 15:24:49 -0400

    Minor addition to the interference calculation.

--- amplitudes/interference_contribution.py
+++ amplitudes/interference_contribution.py
@@ -39,6 +39,99 @@ def calculate_interference_contribution_longitudinally_polarized(
     compton_form_factor_e_tilde_imaginary_part: float,
     verbose: bool = False) -> float:
     """
+    # Title: `calculate_interference_contribution_longitudinally_polarized`
+
+    ## Description:
+    We calculated the BKM10-predicted contribution of the interference between
+    the BH and DVCS process.
+
+    ## Arguments:
+    
+        lepton_polarization: (float)
+
+        target_polarization: (float)
+
+        squared_Q_momentum_transfer: (float)
+
+        x_Bjorken: (float)
+
+        squared_hadronic_momentum_transfer_t: (float)
+
+        lab_kinematics_k: (float)
+
+        azimuthal_phi: (float)
+
+        epsilon:  (float)
+
+        lepton_energy_fraction_y: (float)
+
+        skewness_parameter: (float)
+
+        t_prime: (float)
+
+        k_tilde: (float)
+
+        shorthand_k: (float)
+
+        lepton_propagator_p1: (float)
+
+        lepton_propagator_p2: (float)
+
+        Dirac_form_factor_F1: (float)
+
+        Pauli_form_factor_F2: (float)
+
+        compton_form_factor_h_real_part: (float)
+
+            The real part of the Compton Form Factor (CFF) called H. We will
+            write this as Re[H].
+
+        compton_form_factor_h_tilde_real_part: (float)
+
+            The real part of the Compton Form Factor (CFF) called Ht (H-tilde). We will
+            write this as Re[HT].
+
+        compton_form_factor_e_real_part: (float)
+
+            The real part of the Compton Form Factor (CFF) called E. We will
+            write this as Re[E].
+
+        compton_form_factor_e_tilde_real_part: (float)
+
+            The real part of the Compton Form Factor (CFF) called Et (E-tilde). We will
+            write this as Re[Et].
+
+        compton_form_factor_h_imaginary_part: (float)
+
+            The imaginary/complex part of the Compton Form Factor (CFF) called H. We will
+            write this as Re[H].
+
+        compton_form_factor_h_tilde_imaginary_part: (float)
+
+            The imaginary/complex part of the Compton Form Factor (CFF) called Ht (H-tilde). We will
+            write this as Re[HT].
+
+        compton_form_factor_e_imaginary_part: (float)
+
+            The imaginary/complex part of the Compton Form Factor (CFF) called E. We will
+            write this as Re[E].
+
+        compton_form_factor_e_tilde_imaginary_part: (float)
+
+            The imaginary/complex part of the Compton Form Factor (CFF) called Et (E-tilde). We will
+            write this as Re[Et].
+
+        verbose: (bool)
+
+    ## Returns:
+    
+        bkm10_cross_section_in_nb_GeV4: (float)
+
+            The four-fold differential cross section.
+
+    ## Notes:
+
+    ## Examples:
     """
 
     try:
