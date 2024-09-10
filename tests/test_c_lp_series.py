8a940925e847c1a2507984e0b341c12f9f539d03
ErenMcLaren <dima.watkins@gmail.com>
2024-09-10 12:12:24 -0400

    Added a test for CLPVPP0.

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -1,6 +1,8 @@
 import unittest
 
 from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cpp0 import calculate_c_0_plus_plus_longitudinally_polarized
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp0V import calculate_c_0_plus_plus_longitudinally_polarized_V
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp0A import calculate_c_0_plus_plus_longitudinally_polarized_A
 
 class TestCLPSeriesCoefficients(unittest.TestCase):
 
@@ -15,7 +17,20 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
                 0.477109,
                 0.491757,
                 0.157396,
-                False
-            ), 
-            0.119359
+                False), 
+            0.119359)
+        
+    def test_calculate_c_0_plus_plus_longitudinally_polarized_V(self):
+        self.assertAlmostEqual(
+            calculate_c_0_plus_plus_longitudinally_polarized_V(
+                1.0,
+                1.0,
+                1.82,
+                0.343,
+                -0.172,
+                0.477109,
+                0.491757,
+                0.157396,
+                False), 
+            -0.22258
         )
