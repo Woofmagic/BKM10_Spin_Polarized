e8f385155820e6fe3fb5e40e89b613d64acf84d0
ErenMcLaren <dima.watkins@gmail.com>
2024-09-10 12:35:45 -0400

    Initialized the unit test for the S Series coefficients and wrote a single test for the SLPPP1 coefficient.

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -7,6 +7,22 @@ from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp0A impo
 class TestCLPSeriesCoefficients(unittest.TestCase):
 
     def test_calculate_c_0_plus_plus_longitudinally_polarized(self):
+        """
+        # Title: `test_calculate_c_0_plus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_c_0_plus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP}(n = 0)$.
+        We call it "CLPPP0" for C (series) LP (longitudinally polarized [target]) PP (++) 0 (n = 0).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized(
                 1.0,
@@ -21,6 +37,22 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
             0.119359)
         
     def test_calculate_c_0_plus_plus_longitudinally_polarized_V(self):
+        """
+        # Title: `calculate_c_0_plus_plus_longitudinally_polarized_V`
+
+        ## Description: Test the function `calculate_c_0_plus_plus_longitudinally_polarized_V`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP, V}(n = 0)$.
+        We call it "CLPVPP0" for C (series) LP (longitudinally polarized [target]) V (vector) PP (++) 0 (n = 0).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized_V(
                 1.0,
@@ -35,6 +67,22 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
             -0.22258)
         
     def test_calculate_c_0_plus_plus_longitudinally_polarized_A(self):
+        """
+        # Title: `calculate_c_0_plus_plus_longitudinally_polarized_A`
+
+        ## Description: Test the function `calculate_c_0_plus_plus_longitudinally_polarized_A`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP, A}(n = 0)$.
+        We call it "CLPVPP0" for C (series) LP (longitudinally polarized [target]) A (axial-vector) PP (++) 0 (n = 0).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized_A(
                 1.0,
---
+++ tests/test_s_lp_series.py
@@ -0,0 +1,34 @@
+import unittest
+
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp1 import calculate_s_1_plus_plus_longitudinally_polarized
+
+class TestSLPSeriesCoefficients(unittest.TestCase):
+
+    def test_calculate_s_1_plus_plus_longitudinally_polarized(self):
+        """
+        # Title: `calculate_s_1_plus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_s_1_plus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{++}^{LP}(n = 1)$.
+        We call it "SLPPP1" for S (series) LP (longitudinally polarized [target]) PP (++) 0 (n = 1).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_1_plus_plus_longitudinally_polarized(
+                1.0,
+                1.82,
+                0.343,
+                -0.172,
+                0.477109,
+                0.491757,
+                0.0842939,
+                False), 
+            0.645538)
