5e6bcabdd874d562eec5ccf73de8930d407ae03e
ErenMcLaren <dima.watkins@gmail.com>
2024-09-10 14:25:04 -0400

    Added a test for CLPPP1.

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -4,6 +4,23 @@ from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cpp0 impor
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp0V import calculate_c_0_plus_plus_longitudinally_polarized_V
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp0A import calculate_c_0_plus_plus_longitudinally_polarized_A
 
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp1 import calculate_c_1_plus_plus_longitudinally_polarized
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp1V import calculate_c_1_plus_plus_longitudinally_polarized_V
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Cpp1A import calculate_c_1_plus_plus_longitudinally_polarized_A
+
+_TEST_LEPTON_POLARIZATION = 1.0
+_TEST_TARGET_POLARIZATION = 1.0
+_TEST_SQUARED_Q_MOMENTUM_TRANSFER = 1.8200000524520876
+_TEST_X_BJORKEN = 0.3429999947547912
+_TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER = -0.1720000058412552
+_TEST_EPSILON = 0.477109
+_TEST_LEPTON_ENERGY_FRACTION = 0.491757
+_TEST_K_TILDE = 0.157396
+_TEST_SHORTHAND_K = 0.0842939
+_TEST_T_PRIME = -0.0337889
+
+_TEST_VERBOSE = False
+
 class TestCLPSeriesCoefficients(unittest.TestCase):
 
     def test_calculate_c_0_plus_plus_longitudinally_polarized(self):
@@ -25,15 +42,15 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
         """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized(
-                1.0,
-                1.0,
-                1.8200000524520876,
-                0.3429999947547912,
-                -0.1720000058412552,
-                0.477109,
-                0.491757,
-                0.157396,
-                False), 
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_K_TILDE,
+                _TEST_VERBOSE), 
             0.119359)
         
     def test_calculate_c_0_plus_plus_longitudinally_polarized_V(self):
@@ -55,15 +72,15 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
         """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized_V(
-                1.0,
-                1.0,
-                1.8200000524520876,
-                0.3429999947547912,
-                -0.1720000058412552,
-                0.477109,
-                0.491757,
-                0.157396,
-                False), 
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_K_TILDE,
+                _TEST_VERBOSE), 
             -0.22258)
         
     def test_calculate_c_0_plus_plus_longitudinally_polarized_A(self):
@@ -85,13 +102,43 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
         """
         self.assertAlmostEqual(
             calculate_c_0_plus_plus_longitudinally_polarized_A(
-                1.0,
-                1.0,
-                1.8200000524520876,
-                0.3429999947547912,
-                -0.1720000058412552,
-                0.477109,
-                0.491757,
-                0.157396,
-                False), 
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_K_TILDE,
+                _TEST_VERBOSE), 
             -0.0418893)
+        
+    def test_calculate_c_1_plus_plus_longitudinally_polarized(self):
+        """
+        # Title: `test_calculate_c_1_plus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_c_1_plus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP}(n = 1)$.
+        We call it "CLPPP1" for C (series) LP (longitudinally polarized [target]) PP (++) 0 (n = 1).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_c_1_plus_plus_longitudinally_polarized(
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_SHORTHAND_K,
+                _TEST_VERBOSE), 
+            -0.278046)
