f4764ae85a9f856efd03830a3682836f4601f313
ErenMcLaren <dima.watkins@gmail.com>
2024-09-11 10:37:11 -0400

    Added all the tests for the S++ series coefficients.

--- tests/test_s_lp_series.py
+++ tests/test_s_lp_series.py
@@ -8,6 +8,10 @@ from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp2 impor
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp2V import calculate_s_2_plus_plus_longitudinally_polarized_V
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp2A import calculate_s_2_plus_plus_longitudinally_polarized_A
 
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp3 import calculate_s_3_plus_plus_longitudinally_polarized
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp3V import calculate_s_3_plus_plus_longitudinally_polarized_V
+from coefficients.interference_coefficients.lp_polarized.pl_polarized_Spp3A import calculate_s_3_plus_plus_longitudinally_polarized_A
+
 _TEST_LEPTON_POLARIZATION = 1.0
 _TEST_TARGET_POLARIZATION = 1.0
 _TEST_SQUARED_Q_MOMENTUM_TRANSFER = 1.8200000524520876
@@ -200,3 +204,92 @@ class TestSLPSeriesCoefficients(unittest.TestCase):
                 _TEST_K_TILDE,
                 _TEST_VERBOSE), 
             -0.0047273060125334)
+        
+    def test_calculate_s_3_plus_plus_longitudinally_polarized(self):
+        """
+        # Title: `calculate_s_3_plus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_s_3_plus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{++}^{LP}(n = 3)$.
+        We call it "SLPPP3" for S (series) LP (longitudinally polarized [target]) PP (++) 3 (n = 3).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_3_plus_plus_longitudinally_polarized(
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_T_PRIME,
+                _TEST_SHORTHAND_K,
+                _TEST_VERBOSE), 
+            0.00025690082629607)
+        
+    def test_calculate_s_3_plus_plus_longitudinally_polarized_V(self):
+        """
+        # Title: `test_calculate_s_3_plus_plus_longitudinally_polarized_V`
+
+        ## Description: Test the function `calculate_s_3_plus_plus_longitudinally_polarized_V`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{++}^{LP, V}(n = 3)$.
+        We call it "SLPPP3" for S (series) LP (longitudinally polarized [target]) V (vector) PP (++) 3 (n = 3).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_3_plus_plus_longitudinally_polarized_V(
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_T_PRIME,
+                _TEST_SHORTHAND_K,
+                _TEST_VERBOSE), 
+            0.00017851247673089)
+        
+    def test_calculate_s_3_plus_plus_longitudinally_polarized_A(self):
+        """
+        # Title: `test_calculate_s_3_plus_plus_longitudinally_polarized_A`
+
+        ## Description: Test the function `calculate_s_3_plus_plus_longitudinally_polarized_A`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{++}^{LP, A}(n = 3)$.
+        We call it "SLPPP3" for S (series) LP (longitudinally polarized [target]) A (axial vector) PP (++) 3 (n = 3).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_3_plus_plus_longitudinally_polarized_A(
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_T_PRIME,
+                _TEST_K_TILDE,
+                _TEST_VERBOSE), 
+            0.00015423393697809)
