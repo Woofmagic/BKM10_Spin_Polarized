27257b0dc633bd6e0c8998b655fbc82ac601826f
ErenMcLaren <dima.watkins@gmail.com>
2024-09-12 13:04:34 -0400

    Added a new test for C++LPV2 coefficient.

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -225,8 +225,8 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
         # Title: `test_calculate_c_2_plus_plus_longitudinally_polarized`
 
         ## Description: Test the function `calculate_c_2_plus_plus_longitudinally_polarized`.
-        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP, A}(n = 1)$.
-        We call it "CLPAPP1" for C (series) LP (longitudinally polarized [target]) A (axial vector) PP (++) 0 (n = 1).
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP}(n = 2)$.
+        We call it "CLPPP2" for C (series) LP (longitudinally polarized [target]) PP (++) 2 (n = 2).
 
         ## Arguments:
         None
@@ -248,6 +248,36 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
                 _TEST_LEPTON_ENERGY_FRACTION,
                 _TEST_VERBOSE), 
             0.0024140921642499)
+        
+    def test_calculate_c_2_plus_plus_longitudinally_polarized_V(self):
+        
+        """
+        # Title: `test_calculate_c_2_plus_plus_longitudinally_polarized_V`
+
+        ## Description: Test the function `calculate_c_2_plus_plus_longitudinally_polarized_V`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{++}^{LP, V}(n = 2)$.
+        We call it "CLPVPP2" for C (series) LP (longitudinally polarized [target]) V (vector) PP (++) 2 (n = 2).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_c_2_plus_plus_longitudinally_polarized_V(
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_VERBOSE), 
+            -0.0028267427682111)
         
     def test_calculate_c_0_zero_plus_longitudinally_polarized(self):
         """
