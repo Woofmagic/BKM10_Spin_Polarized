17fd4fa214e32af59b7c50dfaf24a987d75670c2
ErenMcLaren <dima.watkins@gmail.com>
2024-09-23 15:12:50 -0400

    Added a new test for S-+(n = 1).

--- tests/test_s_lp_series.py
+++ tests/test_s_lp_series.py
@@ -20,6 +20,10 @@ from coefficients.interference_coefficients.lp_polarized.pl_polarized_S0p2 impor
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_S0p2V import calculate_s_2_zero_plus_longitudinally_polarized_V
 from coefficients.interference_coefficients.lp_polarized.pl_polarized_S0p2A import calculate_s_2_zero_plus_longitudinally_polarized_A
 
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Smp1 import calculate_s_1_minus_plus_longitudinally_polarized
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Smp1V import calculate_s_1_minus_plus_longitudinally_polarized_V
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Smp1A import calculate_s_1_minus_plus_longitudinally_polarized_A
+
 _TEST_LEPTON_POLARIZATION = 1.0
 _TEST_TARGET_POLARIZATION = 1.0
 _TEST_SQUARED_Q_MOMENTUM_TRANSFER = 1.8200000524520876
@@ -474,3 +478,32 @@ class TestSLPSeriesCoefficients(unittest.TestCase):
                 _TEST_SHORTHAND_K,
                 _TEST_VERBOSE), 
             0.017779238335062)
+        
+    def test_calculate_s_1_minus_plus_longitudinally_polarized(self):
+        """
+        # Title: `test_calculate_s_1_minus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_s_1_minus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{-+}^{LP}(n = 1)$.
+        We call it "SLPPP1" for S (series) LP (longitudinally polarized [target]) MP (-+) 1 (n = 1).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_1_minus_plus_longitudinally_polarized(
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_SHORTHAND_K,
+                _TEST_VERBOSE), 
+            -0.15053635153596)
