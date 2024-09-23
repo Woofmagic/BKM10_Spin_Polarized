195c7aace2636abb4511e82cd46d73111f93b3ed
ErenMcLaren <dima.watkins@gmail.com>
2024-09-23 13:42:25 -0400

    Added a test for C-+A(n = 0).

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -612,3 +612,32 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
                 _TEST_K_TILDE,
                 _TEST_VERBOSE), 
             0.000089774829475794)
+        
+    def test_calculate_c_0_minus_plus_longitudinally_polarized_A(self):
+        """
+        # Title: `test_calculate_c_0_minus_plus_longitudinally_polarized_A`
+
+        ## Description: Test the function `calculate_c_0_minus_plus_longitudinally_polarized_A`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{-+}^{LP, A}(n = 0)$.
+        We call it "CLPVMP0" for C (series) LP (longitudinally polarized [target]) A (axial vector) MP (-+) 0 (n = 0).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_c_0_minus_plus_longitudinally_polarized_A(
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_VERBOSE), 
+            0.0028441866111567)
