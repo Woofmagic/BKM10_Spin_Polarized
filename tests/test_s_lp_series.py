71ac13c68e4459b30c27e52f180ab247023606c2
ErenMcLaren <dima.watkins@gmail.com>
2024-09-23 16:50:40 -0400

    Added a test for S-+A(n = 2).

--- tests/test_s_lp_series.py
+++ tests/test_s_lp_series.py
@@ -626,3 +626,32 @@ class TestSLPSeriesCoefficients(unittest.TestCase):
                 _TEST_K_TILDE,
                 _TEST_VERBOSE), 
             0.87645317970911)
+        
+    def test_calculate_s_2_minus_plus_longitudinally_polarized_A(self):
+        """
+        # Title: `test_calculate_s_2_minus_plus_longitudinally_polarized_A`
+
+        ## Description: Test the function `calculate_s_2_minus_plus_longitudinally_polarized_A`.
+        Remember, that function corresponds to the BKM10 coefficient called $S_{-+}^{LP,V}(n = 2)$.
+        We call it "SLPVPP2" for S (series) LP (longitudinally polarized [target]) A (axial vector) MP (-+) 2 (n = 2).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_s_2_minus_plus_longitudinally_polarized_A(
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_K_TILDE,
+                _TEST_VERBOSE), 
+            0.11404596503142)
