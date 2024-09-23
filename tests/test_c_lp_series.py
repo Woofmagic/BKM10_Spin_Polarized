911a9d65f638faf6456b4ccffbef548865ab4922
ErenMcLaren <dima.watkins@gmail.com>
2024-09-23 13:57:45 -0400

    Added a test for C-+(n=1).

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -27,6 +27,10 @@ from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp0 impor
 from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp0V import calculate_c_0_minus_plus_longitudinally_polarized_V
 from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp0A import calculate_c_0_minus_plus_longitudinally_polarized_A
 
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp1 import calculate_c_1_minus_plus_longitudinally_polarized
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp1V import calculate_c_1_minus_plus_longitudinally_polarized_V
+from coefficients.interference_coefficients.lp_polarized.lp_polarized_Cmp1A import calculate_c_1_minus_plus_longitudinally_polarized_A
+
 _TEST_LEPTON_POLARIZATION = 1.0
 _TEST_TARGET_POLARIZATION = 1.0
 _TEST_SQUARED_Q_MOMENTUM_TRANSFER = 1.8200000524520876
@@ -641,3 +645,33 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
                 _TEST_LEPTON_ENERGY_FRACTION,
                 _TEST_VERBOSE), 
             0.0028441866111567)
+        
+    def test_calculate_c_1_minus_plus_longitudinally_polarized(self):
+        """
+        # Title: `test_calculate_c_1_minus_plus_longitudinally_polarized`
+
+        ## Description: Test the function `calculate_c_1_minus_plus_longitudinally_polarized`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{-+}^{LP}(n = 0)$.
+        We call it "CLPMP0" for C (series) LP (longitudinally polarized [target]) MP (-+) 0 (n = 0).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_c_1_minus_plus_longitudinally_polarized(
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_SHORTHAND_K,
+                _TEST_VERBOSE), 
+            -0.063674515630947)
