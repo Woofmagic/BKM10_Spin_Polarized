8ec6ca1e2bb2599a2dfcd9b7c28373e6ec01b388
ErenMcLaren <dima.watkins@gmail.com>
2024-09-23 14:25:30 -0400

    Added a test for C-+V(n=2).

--- tests/test_c_lp_series.py
+++ tests/test_c_lp_series.py
@@ -766,3 +766,32 @@ class TestCLPSeriesCoefficients(unittest.TestCase):
                 _TEST_LEPTON_ENERGY_FRACTION,
                 _TEST_VERBOSE), 
             -0.18590121965401)
+        
+    def test_calculate_c_2_minus_plus_longitudinally_polarized_V(self):
+        """
+        # Title: `test_calculate_c_2_minus_plus_longitudinally_polarized_V`
+
+        ## Description: Test the function `calculate_c_2_minus_plus_longitudinally_polarized_V`.
+        Remember, that function corresponds to the BKM10 coefficient called $C_{-+}^{LP, V}(n = 2)$.
+        We call it "CLPVMP2" for C (series) LP (longitudinally polarized [target]) V (vector) MP (-+) 2 (n = 2).
+
+        ## Arguments:
+        None
+
+        ## Returns:
+        None
+
+        ## Examples:
+        None
+        """
+        self.assertAlmostEqual(
+            calculate_c_2_minus_plus_longitudinally_polarized_V(
+                _TEST_LEPTON_POLARIZATION,
+                _TEST_TARGET_POLARIZATION,
+                _TEST_SQUARED_Q_MOMENTUM_TRANSFER,
+                _TEST_X_BJORKEN,
+                _TEST_SQUARED_HADRONIC_MOMENTUM_TRANSFER,
+                _TEST_EPSILON,
+                _TEST_LEPTON_ENERGY_FRACTION,
+                _TEST_VERBOSE), 
+            0.21767807213023)
