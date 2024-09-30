43643085a7309da2733fb6ff2289c43f38a9ac30
ErenMcLaren <dima.watkins@gmail.com>
2024-09-30 12:31:48 -0400

    Updated the function that computes CPpp1V. Looks good now.

--- coefficients/interference_coefficients/unpolarized/unpolarized_Cpp1V.py
+++ coefficients/interference_coefficients/unpolarized/unpolarized_Cpp1V.py
@@ -9,7 +9,8 @@ def calculate_c_1_plus_plus_unpolarized_V(
     squared_hadronic_momentum_transfer_t: float,
     epsilon: float,
     lepton_energy_fraction_y: float, 
-    k_tilde: float,
+    t_prime: float,
+    shorthand_k: float,
     verbose: bool = False) -> float:
     """
     """
@@ -22,40 +23,28 @@ def calculate_c_1_plus_plus_unpolarized_V(
         # (2): Calculate the recurrent quantity t/Q^{2}:
         t_over_Q_squared = squared_hadronic_momentum_transfer_t / squared_Q_momentum_transfer
 
-        # (3): Calculate the recurrent quantity 1 + sqrt(1 + epsilon^2):
-        one_plus_root_epsilon_stuff = 1. + root_one_plus_epsilon_squared
+        # (3): Calculate the first bracket term:
+        first_bracket_term = (2. - lepton_energy_fraction_y)**2 * (1. - (1. - 2. * x_Bjorken) * t_over_Q_squared)
 
-        # (4): Compute the first term in the brackets:
-        first_term_in_brackets = (2. - lepton_energy_fraction_y)**2 * k_tilde**2 / (root_one_plus_epsilon_squared * squared_Q_momentum_transfer)
+        # (4): Compute the first part of the second term in brackets:
+        second_bracket_term_first_part = 1. - lepton_energy_fraction_y - epsilon**2 * lepton_energy_fraction_y**2 / 4.
 
-        # (5): First multiplicative term in the second term in the brackets:
-        second_term_first_multiplicative_term = 1. - lepton_energy_fraction_y - (epsilon**2 * lepton_energy_fraction_y**2 / 4.)
+        # (5): Compute the second part of the second term in brackets:
+        second_bracket_term_second_part = 0.5 * (1. + root_one_plus_epsilon_squared - 2. * x_Bjorken) * t_prime / squared_Q_momentum_transfer
 
-        # (6): Second multiplicative term in the second term in the brackets:
-        second_term_second_multiplicative_term = one_plus_root_epsilon_stuff / 2.
+        # (6): The prefactor in front of the brackets:
+        coefficient_prefactor = 16. * shorthand_k * x_Bjorken * t_over_Q_squared / root_one_plus_epsilon_squared**2.5
 
-        # (7): Third multiplicative term in the second term in the brackets:
-        second_term_third_multiplicative_term = 1. + t_over_Q_squared
+        # (7): The entire thing:
+        c_1_plus_plus_V_unp = coefficient_prefactor * (first_bracket_term + second_bracket_term_first_part * second_bracket_term_second_part)
 
-        # (8): Fourth multiplicative term numerator in the second term in the brackets:
-        second_term_fourth_multiplicative_term = 1. + (root_one_plus_epsilon_squared - 1. + (2. * x_Bjorken)) * t_over_Q_squared / one_plus_root_epsilon_stuff
-
-        # (9): Fourth multiplicative term in its entirety:
-        second_term_in_brackets = second_term_first_multiplicative_term * second_term_second_multiplicative_term * second_term_third_multiplicative_term * second_term_fourth_multiplicative_term
-
-        # (10): The prefactor in front of the brackets:
-        coefficient_prefactor = 8. * (2. - lepton_energy_fraction_y) * x_Bjorken * t_over_Q_squared / root_one_plus_epsilon_squared**4
-
-        # (11): The entire thing:
-        c_0_plus_plus_V_unp = coefficient_prefactor * (first_term_in_brackets + second_term_in_brackets)
-
-        # (11.1): If verbose, log the output:
+        # (7.1): If verbose, log the output:
         if verbose:
-            print(f"> Calculated c_0_plus_plus_V_unp to be:\n{c_0_plus_plus_V_unp}")
+            print(f"> Calculated c_1_plus_plus_V_unp to be:\n{c_1_plus_plus_V_unp}")
 
-        # (12): Return the coefficient:
-        return c_0_plus_plus_V_unp
+        # (8): Return the coefficient:
+        return c_1_plus_plus_V_unp
 
     except Exception as ERROR:
-        print(f"> Error in calculating c_0_plus_plus_V_unp for Interference Term:\n> {ERROR}")
+        print(f"> Error in calculating c_1_plus_plus_V_unp for Interference Term:\n> {ERROR}")
         return 0.
