e3f45c698a2ee56de787d68a5917f76c93643f75
ErenMcLaren <dima.watkins@gmail.com>
2024-10-02 16:30:38 -0400

    Added a function for Sunp0P2A.

---
+++ coefficients/interference_coefficients/unpolarized/unpolarized_S0p2A.py
@@ -0,0 +1,50 @@
+try:
+    import numpy as np
+except ImportError:
+    print("NumPy is not installed. Please install NumPy to use this script.")
+
+def calculate_s_2_zero_plus_unpolarized_A(
+    squared_Q_momentum_transfer: float, 
+    x_Bjorken: float, 
+    squared_hadronic_momentum_transfer_t: float,
+    epsilon: float,
+    lepton_energy_fraction_y: float, 
+    t_prime: float,
+    shorthand_k: float,
+    verbose: bool = False) -> float:
+    """
+    """
+
+    try:
+
+        # (1): Calculate the recurrent quantity sqrt(1 + epsilon^2):
+        root_one_plus_epsilon_squared = np.sqrt(1. + epsilon**2)
+
+        # (2): Calculate the recurrent quantity t/Q^{2}:
+        t_over_Q_squared = squared_hadronic_momentum_transfer_t / squared_Q_momentum_transfer
+
+        # (3): Calculate 1 - x_{B}:
+        one_minus_xb = 1. - x_Bjorken
+
+        # (4): Calculate the annoying y quantity:
+        y_quantity = 1. - lepton_energy_fraction_y - (epsilon**2 * lepton_energy_fraction_y**2 / 4.)
+
+        # (5): Calculate the main term:
+        main_term = 4. * one_minus_xb + 2. * epsilon**2 + 4. * t_over_Q_squared * (4. * x_Bjorken * one_minus_xb + epsilon**2)
+        
+        # (6): Calculate part of the prefactor:
+        prefactor = 8. * np.sqrt(2. * y_quantity) * shorthand_k * (2. - lepton_energy_fraction_y) * t_over_Q_squared / root_one_plus_epsilon_squared**4
+        
+        # (7): Calculate the coefficient:
+        c_2_zero_plus_unp_A = prefactor * main_term
+        
+        # (7.1): If verbose, log the output:
+        if verbose:
+            print(f"> Calculated c_2_zero_plus_unp_A to be:\n{c_2_zero_plus_unp_A}")
+
+        # (8): Return the coefficient:
+        return c_2_zero_plus_unp_A
+
+    except Exception as ERROR:
+        print(f"> Error in calculating c_2_zero_plus_unp_A for Interference Term:\n> {ERROR}")
+        return 0.
