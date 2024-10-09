a915d547e4808b3f669b9d1ff3ab1003336fa4f6
ErenMcLaren <dima.watkins@gmail.com>
2024-10-09 12:48:11 -0400

    Added more details for the coefficient analysis.

--- analysis/coefficient_analysis.py
+++ analysis/coefficient_analysis.py
@@ -1,27 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-results = np.random.rand(100)
-
 fourier_coefficient_labels = {
     'unpolarized_coefficient_plus_plus_n_equals_0': r'$C_{++}^{unp} \left ( n = 0 \right)$',
     'unpolarized_coefficient_plus_plus_n_equals_1': r'$C_{++}^{unp} \left ( n = 1 \right)$',
     'unpolarized_coefficient_plus_plus_n_equals_2': r'$C_{++}^{unp} \left ( n = 2 \right)$',
+    'unpolarized_coefficient_plus_plus_n_equals_3': r'$C_{++}^{unp} \left ( n = 3 \right)$',
+    'unpolarized_coefficient_plus_plus_V_n_equals_0': r'$C_{++}^{unp, V} \left ( n = 0 \right)$',
+    'unpolarized_coefficient_plus_plus_V_n_equals_1': r'$C_{++}^{unp, V} \left ( n = 1 \right)$',
+    'unpolarized_coefficient_plus_plus_V_n_equals_2': r'$C_{++}^{unp, V} \left ( n = 2 \right)$',
+    'unpolarized_coefficient_plus_plus_V_n_equals_3': r'$C_{++}^{unp, V} \left ( n = 3 \right)$',
+    'unpolarized_coefficient_plus_plus_A_n_equals_0': r'$C_{++}^{unp, A} \left ( n = 0 \right)$',
+    'unpolarized_coefficient_plus_plus_A_n_equals_1': r'$C_{++}^{unp, A} \left ( n = 1 \right)$',
+    'unpolarized_coefficient_plus_plus_A_n_equals_2': r'$C_{++}^{unp, A} \left ( n = 2 \right)$',
+    'unpolarized_coefficient_plus_plus_A_n_equals_3': r'$C_{++}^{unp, A} \left ( n = 3 \right)$',
+    'unpolarized_coefficient_zero_plus_n_equals_0': r'$C_{0+}^{unp} \left ( n = 0 \right)$',
+    'unpolarized_coefficient_zero_plus_n_equals_1': r'$C_{0+}^{unp} \left ( n = 1 \right)$',
+    'unpolarized_coefficient_zero_plus_n_equals_2': r'$C_{0+}^{unp} \left ( n = 2 \right)$',
+    'unpolarized_coefficient_zero_plus_V_n_equals_0': r'$C_{0+}^{unp, V} \left ( n = 0 \right)$',
+    'unpolarized_coefficient_zero_plus_V_n_equals_1': r'$C_{0+}^{unp, V} \left ( n = 1 \right)$',
+    'unpolarized_coefficient_zero_plus_V_n_equals_2': r'$C_{0+}^{unp, V} \left ( n = 2 \right)$',
+    'unpolarized_coefficient_zero_plus_A_n_equals_0': r'$C_{0+}^{unp, A} \left ( n = 0 \right)$',
+    'unpolarized_coefficient_zero_plus_A_n_equals_1': r'$C_{0+}^{unp, A} \left ( n = 1 \right)$',
+    'unpolarized_coefficient_zero_plus_A_n_equals_2': r'$C_{0+}^{unp, A} \left ( n = 2 \right)$',
+
 }
 
 histogram_labels = [
     fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_0'], 
-    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_0'], 
-    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_0']
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_1'], 
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_2'],
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_n_equals_3'],
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_V_n_equals_1'], 
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_V_n_equals_2'], 
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_V_n_equals_3'],
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_A_n_equals_0'],
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_A_n_equals_1'], 
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_A_n_equals_2'], 
+    fourier_coefficient_labels['unpolarized_coefficient_plus_plus_A_n_equals_3'],
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_n_equals_0'],
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_n_equals_1'], 
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_n_equals_2'], 
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_V_n_equals_0'],
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_V_n_equals_1'],
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_V_n_equals_2'], 
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_A_n_equals_0'], 
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_A_n_equals_1'],
+    fourier_coefficient_labels['unpolarized_coefficient_zero_plus_A_n_equals_2'],
 ]
+
+results = np.random.rand(len(histogram_labels))
 
 plt.figure(figsize=(10, 6))
 plt.bar(
     x = range(len(results)), 
-    heights = results, 
-    tick_label = histogram_labels)
+    height = results, 
+    tick_label = histogram_labels,
+    color = plt.get_cmap("viridis").colors)
 
-plt.xlabel('Subfunction')
-plt.ylabel('Result')
-plt.title(r'UnpolarizedFourier Coefficient Analysis, $C_{\alpha \beta}^{unp} \left( n \right)$')
+plt.xticks(rotation = 45)
+plt.xlabel('Fourier Coefficients')
+plt.ylabel('Numerical Value [dimensionless]')
+plt.title(r'Unpolarized Fourier Coefficient Analysis, $C_{\alpha \beta}^{unp} \left( n \right)$')
 plt.show()
