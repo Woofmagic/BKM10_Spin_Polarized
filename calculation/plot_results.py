Major update in specifically the calculation of S(n=1)++: WRONG argument was being passed: t instead of t'.

calculation/plot_results.py
@@ -101,37 +101,37 @@ def plot_interference_contributions(
     interference_coefficients_plot.add_line_plot(
         x_data = lab_azimuthal_phi,
         y_data = c1Interference_amplitude_contribution * np.array([np.cos(1. * (np.pi - convert_degrees_to_radians(phi))) for phi in lab_azimuthal_phi]),
-        label = r"$c_{1} \np.cos(\pi - \phi)$",
+        label = r"$c_{1} \cos(\pi - \phi)$",
         color = 'orange')
     
     interference_coefficients_plot.add_line_plot(
             x_data = lab_azimuthal_phi,
             y_data = c2Interference_amplitude_contribution * np.array([np.cos(2. * (np.pi - convert_degrees_to_radians(phi))) for phi in lab_azimuthal_phi]),
-            label = r"$c_{2} \np.cos(2 (\pi - \phi))$",
+            label = r"$c_{2} \cos(2 (\pi - \phi))$",
             color = 'yellow')
         
     interference_coefficients_plot.add_line_plot(
         x_data = lab_azimuthal_phi,
         y_data = c3Interference_amplitude_contribution * np.array([np.cos(3. * (np.pi - convert_degrees_to_radians(phi))) for phi in lab_azimuthal_phi]),
-        label = r"$c_{3} \np.cos(3 (\pi - \phi))$",
+        label = r"$c_{3} \cos(3 (\pi - \phi))$",
         color = 'green')
     
     interference_coefficients_plot.add_line_plot(
         x_data = lab_azimuthal_phi,
-        y_data = s1Interference_amplitude_contribution * np.np.sin(np.pi - convert_degrees_to_radians(lab_azimuthal_phi)),
-        label = r"$s_{1} \np.sin(\pi - \phi)$",
+        y_data = s1Interference_amplitude_contribution * np.sin(np.pi - convert_degrees_to_radians(lab_azimuthal_phi)),
+        label = r"$s_{1} \in(\pi - \phi)$",
         color = 'cyan')
     
     interference_coefficients_plot.add_line_plot(
         x_data = lab_azimuthal_phi,
-        y_data = s2Interference_amplitude_contribution * np.np.sin(2. * (np.pi - convert_degrees_to_radians(lab_azimuthal_phi))),
-        label = r"$s_{2} \np.sin(2 (\pi - \phi))$",
+        y_data = s2Interference_amplitude_contribution * np.sin(2. * (np.pi - convert_degrees_to_radians(lab_azimuthal_phi))),
+        label = r"$s_{2} \sin(2 (\pi - \phi))$",
         color = 'blue')
         
     interference_coefficients_plot.add_line_plot(
         x_data = lab_azimuthal_phi,
-        y_data = s3Interference_amplitude_contribution * np.np.sin(3. * (np.pi - convert_degrees_to_radians(lab_azimuthal_phi))),
-        label = r"$s_{3} \np.sin(3 ( \pi - \phi ))$",
+        y_data = s3Interference_amplitude_contribution * np.sin(3. * (np.pi - convert_degrees_to_radians(lab_azimuthal_phi))),
+        label = r"$s_{3} \sin(3 ( \pi - \phi ))$",
         color = 'purple')
     
     interference_coefficients_contributions = PlotCustomizer(

coefficients/interference_coefficients/unpolarized/unpolarized_Cpp0.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def calculate_c_0_plus_plus_unpolarized(
-    squared_Q_momentum_transfer: float, 
-    x_Bjorken: float, 
+    squared_Q_momentum_transfer: float,
+    x_Bjorken: float,
     squared_hadronic_momentum_transfer_t: float,
     epsilon: float,
     lepton_energy_fraction_y: float,
@@ -29,7 +29,7 @@ def calculate_c_0_plus_plus_unpolarized(
         two_minus_y = 2. - lepton_energy_fraction_y
 
         # (6): Calculate the first term in the brackets:
-        first_term_in_brackets = k_tilde**2 * two_minus_y**2 / (squared_Q_momentum_transfer ** root_one_plus_epsilon_squared)
+        first_term_in_brackets = k_tilde**2 * two_minus_y**2 / (squared_Q_momentum_transfer * root_one_plus_epsilon_squared)
 
         # (7): Calculate the first part of the second term in brackets:
         second_term_in_brackets_first_part = t_over_Q_squared * two_minus_xb * (1. - lepton_energy_fraction_y - (epsilon**2 * lepton_energy_fraction_y**2 / 4.))
@@ -41,7 +41,7 @@ def calculate_c_0_plus_plus_unpolarized(
         second_term_in_brackets_second_part =  1. + second_term_in_brackets_second_part_numerator / (two_minus_xb * one_plus_root_epsilon_stuff)
         
         # (10): Calculate the prefactor:
-        prefactor = 4. * two_minus_y * one_plus_root_epsilon_stuff / np.power(root_one_plus_epsilon_squared, 4)
+        prefactor = -4. * two_minus_y * one_plus_root_epsilon_stuff / np.power(root_one_plus_epsilon_squared, 4)
 
         # (11): Calculate the coefficient
         c_0_plus_plus_unp = prefactor * (first_term_in_brackets + second_term_in_brackets_first_part * second_term_in_brackets_second_part)