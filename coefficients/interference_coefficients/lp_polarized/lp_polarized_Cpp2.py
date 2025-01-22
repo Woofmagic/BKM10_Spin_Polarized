521969ebb2425cee506d232e1d3f98a82b9d30d1
MidnightExcursion <dima.watkins@gmail.com>
2025-01-22 10:29:21 -0600

    Typo in C++LP(n = 2): missing (-) for prefactor.

--- coefficients/interference_coefficients/lp_polarized/lp_polarized_Cpp2.py
+++ coefficients/interference_coefficients/lp_polarized/lp_polarized_Cpp2.py
@@ -30,7 +30,7 @@ def calculate_c_2_plus_plus_longitudinally_polarized(
         second_multiplicative_factor = x_Bjorken * t_over_Q_squared - (epsilon**2 * (1. - t_over_Q_squared) / 2.)
 
         # (6): Calculate the prefactor:
-        prefactor = 4. * lepton_helicity * target_polarization * lepton_energy_fraction_y * (1. - lepton_energy_fraction_y - (lepton_energy_fraction_y**2 * epsilon**2 / 4.)) / root_one_plus_epsilon_squared**5
+        prefactor = -4. * lepton_helicity * target_polarization * lepton_energy_fraction_y * (1. - lepton_energy_fraction_y - (lepton_energy_fraction_y**2 * epsilon**2 / 4.)) / root_one_plus_epsilon_squared**5
 
         # (6): Calculate the entire thing:
         c_2_plus_plus_LP = prefactor * first_multiplicative_factor * second_multiplicative_factor
